9954eda347547ba2fc97a53244acb68509dc1739
dnl-trpp <daniel.trippa@gmail.com>
2023-03-08 21:34:52 +0100

    Added Second Microservices and configured network

--- warehouse_api/warehouse_api.py
+++ warehouse_api/warehouse_api.py
@@ -1,10 +1,13 @@
 from flask import Flask
+import requests
 
 server = Flask(__name__)
 
 @server.route('/')
 def main():
-    response = '<div>   Hello! </div>'
+    response = '<div>   Hello from Warehouse Api!</div>'
+    res = requests.get("http://catalog_api:8000")
+    response += str(res.status_code)
     return response
 
 
